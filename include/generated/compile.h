d602fb8b0cf706e062fb01babee77c1074377cbb
hnhkj <hnhkj@163.com>
2017-04-25 16:14:16 +0800

    Hi3518E_SDK_V1.0.3.0 linux-3.4.y

---
+++ include/generated/compile.h
@@ -0,0 +1,6 @@
+/* This file is auto generated, version 1 */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 Tue Apr 25 14:24:54 CST 2017"
+#define LINUX_COMPILE_BY "ubuntu"
+#define LINUX_COMPILE_HOST "ubuntu-virtual-machine"
+#define LINUX_COMPILER "gcc version 4.8.3 20131202 (prerelease) (Hisilicon_v300) "
---
+++ usr/.initramfs_data.cpio.d
@@ -0,0 +1,1 @@
+deps_initramfs := /home/ubuntu/miniIPC/Hi3518E_SDK_V1.0.3.0/osdrv/opensource/kernel/linux-3.4.y/scripts/gen_initramfs_list.sh \
